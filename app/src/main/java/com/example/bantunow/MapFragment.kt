1330e4521897cb8734904430eeb0821561f4bddd
hzrl04 <hazrilirfan@gmail.com>
2026-06-18 00:43:11 +0800

    Added crash handler for the map webview to help reduce app crashes

--- app/src/main/java/com/example/bantunow/MapFragment.kt
+++ app/src/main/java/com/example/bantunow/MapFragment.kt
@@ -2,9 +2,12 @@ package com.example.bantunow
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.RenderProcessGoneDetail
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.bantunow.databinding.FragmentMapBinding
@@ -29,7 +32,29 @@ class MapFragment : Fragment() {
         // Simple UI-only WebView setup
         binding.mapWebView.settings.javaScriptEnabled = true
         binding.mapWebView.settings.domStorageEnabled = true
-        binding.mapWebView.webViewClient = WebViewClient()
+        binding.mapWebView.webViewClient = object : WebViewClient(){
+            /**
+             * Special handler for map web view client when its rendering process
+             * is stopped
+             * This handles the error gracefully to avoid crashing the entire app
+             */
+            override fun onRenderProcessGone(
+                view: WebView?,
+                detail: RenderProcessGoneDetail?
+            ): Boolean {
+                if (detail?.didCrash() == true){
+                    Log.e("WebView", "Render process crashed!")
+                }
+                else {
+                    Log.e("WebView", "Render process killed by system!")
+                }
+                val parent = view?.parent as? ViewGroup
+                parent?.removeView(view)
+                view?.destroy()
+
+                return true
+            }
+        }
         binding.mapWebView.loadUrl("file:///android_asset/map.html")
     }
 
