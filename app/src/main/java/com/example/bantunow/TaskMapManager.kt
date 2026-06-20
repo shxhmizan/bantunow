81c5efdd814f9c66bc991749ebf755926578b4f9
hzrl04 <hazrilirfan@gmail.com>
2026-06-20 14:52:49 +0800

    Added comments for task map manager

--- app/src/main/java/com/example/bantunow/TaskMapManager.kt
+++ app/src/main/java/com/example/bantunow/TaskMapManager.kt
@@ -18,7 +18,16 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
+/**
+ * This class manages the application Task Map that displays tasks in an interactive map
+ * It manages communication between the application, the Android Webview displaying the map
+ * and the Firebase Database
+ */
 class TaskMapManager(val database: FirebaseDatabase, val locationClient: FusedLocationProviderClient) : WebViewCompat.WebMessageListener {
+    /**
+     * This method processes messages received from the map WebView through JavaScript
+     * The Android applcation may respond back to the map with the requested data in this method
+     */
     @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
     @SuppressLint("RequiresFeature")
     override fun onPostMessage(
@@ -29,11 +38,13 @@ class TaskMapManager(val database: FirebaseDatabase, val locationClient: FusedLo
         replyProxy: JavaScriptReplyProxy
     ) {
         val data = msg.data ?: return
-        //Log.i("MapEventListener", "Event : $data ")
         try {
             val request = Json.decodeFromString<TaskMapRequest>(data);
             Log.i("TaskMapManager", "Request Type : ${request.type}")
             when (request.type) {
+                //Request: Data of nearby tasks
+                //Happens when the map is loaded or reloaded
+                //Response: A JSON-formatted list of nearby tasks
                 TaskMapRequest.Type.GET_NEARBY_TASKS -> {
                     locationClient.lastLocation.addOnCompleteListener { task ->
                         if (task.isSuccessful) replyProxy.postMessage(
@@ -50,6 +61,9 @@ class TaskMapManager(val database: FirebaseDatabase, val locationClient: FusedLo
                     }
                 }
 
+                //Request: Current location
+                //To center the map on the user's location
+                //Response: The location data of the user from device location services
                 TaskMapRequest.Type.GET_CURRENT_LOCATION -> {
                     Task.applyOnNearbyTasks(database) { tasks ->
                         if (tasks == null) return@applyOnNearbyTasks
@@ -63,6 +77,9 @@ class TaskMapManager(val database: FirebaseDatabase, val locationClient: FusedLo
                     }
                 }
 
+                //Request: Task details
+                //When the user clicks a task on the map to view details
+                //Response: JSON-formatted task details
                 TaskMapRequest.Type.GET_TASK_DETAILS -> {
                     val taskID = request.contents ?: return
                     Task.getTaskByID(database, taskID) { task ->
@@ -75,6 +92,7 @@ class TaskMapManager(val database: FirebaseDatabase, val locationClient: FusedLo
                         replyProxy.postMessage(taskJson)
 
                         //At this point need to open the fragment with the task details
+
                     }
                 }
             }
